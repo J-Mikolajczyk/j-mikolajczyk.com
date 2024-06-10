e70397311040d5efc5371efa220bbd37756c409d
John Mikolajczyk <johnpaulmik@gmail.com>
2024-06-09 21:37:30 -0400

    Adding home

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function Home() {
+
+  return (
+    <>
+        <div class="container vertical">
+            <h1>Hi, I'm John</h1>
+        
+            <p class="main-text">I know that my website is a bit of a barren wasteland, but I'm working hard to add new features and beautify it. Stay tuned for updates and improvements!</p>
+        </div>
+    </>
+  )
+}
+
+export default Home
