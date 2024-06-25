e20eebcad4893632a5c2af6bfec2b737ceaf7ad0
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2024-06-25 11:54:16 -0700

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
         <div class="container horizontal footer">
             <div class="footer-item">
                 <a class="footer-link" href="https://www.netlify.com" target="_blank">Netlify</a>
-                <img src="\public\netlify-logo.png" width="25" height="25"></img>
+                <img src="/public/netlify-logo.png" width="25" height="25"></img>
             </div>
             <div class="footer-item">
                 <a class="footer-link" href="https://github.com/" target="_blank">GitHub</a>
-                <img src="\public\github-mark-white.png" width="25" height="25"></img>
+                <img src="/public/github-mark-white.png" width="25" height="25"></img>
             </div>
         </div>
     </footer>
