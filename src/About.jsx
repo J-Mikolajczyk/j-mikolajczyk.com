bf2fd772544d27035ef9571a4cce056167f0fab1
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2024-12-18 06:06:36 -0500

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -8,8 +8,8 @@ function About() {
     
         <h1>About Me</h1>
 
-        <p class="main-text">Hi, I’m John Mikolajczyk, an MBA candidate at Alfred University, where I’m working toward my Master of Business Administration. My goal is to further my education and grow into a leadership role as the lead of a software engineering team. I recently earned my Bachelor of Science in Computer Science from Colorado State University, where I cultivated a strong foundation in technology and problem-solving. </p>
-        <p class="main-text">In addition to staying up-to-date with industry trends, I spend my free time watching educational videos and exploring how technology shapes the business landscape. My creative outlet is weightlifting, and I recently competed in my first powerlifting meet—an experience I can’t wait to repeat. I’m also an avid sports fan who loves volleyball, football, and cheering for the Buffalo Bills.</p>
+        <p class="main-text">Hi, I’m John Mikolajczyk, an MBA candidate at Alfred University, where I’m working toward my Master of Business Administration. My goal is to further my education and grow into a leadership role as the lead of a software engineering team. I recently earned my Bachelor of Science in Computer Science from Colorado State University.</p>
+        <p class="main-text">In my free time, I enjoy playing video games, spending time with family, and weightlifting. I actually recently competed in my first powerlifting meet and I cannot wait to do it again. I’m also an avid sports fan who loves to play volleyball and spend my Sundays cheering for the Buffalo Bills.</p>
         </div>
     </>
   )
