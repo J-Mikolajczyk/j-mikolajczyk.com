cc98c32aa5b536aa1f2e5c43d49535ef34e2710d
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2024-12-18 06:03:11 -0500

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -8,7 +8,8 @@ function About() {
     
         <h1>About Me</h1>
 
-        <p class="main-text">My name is John Mikolajczyk and I am a student working towards my Bachelor of Science in Computer Science at Colorado State University. After completing my degree this fall, I plan to attend graduate school pursuing a Masters of Business Administration.</p>
+        <p class="main-text">Hi, I’m John Mikolajczyk, an MBA candidate at Alfred University, where I’m working toward my Master of Business Administration. My goal is to further my education and grow into a leadership role as the lead of a software engineering team. I recently earned my Bachelor of Science in Computer Science from Colorado State University, where I cultivated a strong foundation in technology and problem-solving. </p>
+        <p class="main-text">In addition to staying up-to-date with industry trends, I spend my free time watching educational videos and exploring how technology shapes the business landscape. My creative outlet is weightlifting, and I recently competed in my first powerlifting meet—an experience I can’t wait to repeat. I’m also an avid sports fan who loves volleyball, football, and cheering for the Buffalo Bills.</p>
         </div>
     </>
   )
