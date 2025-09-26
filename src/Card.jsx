99849aa7985d0fb0b59511c28302f546d827fd90
John Mikolajczyk <johnpaulmik@gmail.com>
2025-09-26 08:27:57 -0400

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,16 @@
 export default function Card({title, desc, link, site}) {
-    const openLink = (link) => {
-        window.open(link, '_blank', 'noopener,noreferrer');
-    };
+  const openLink = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
 
-    return (
-        <div class="project-card">
-            <h3 class="project-title">{title}</h3>
-            <p class="project-desc">{desc}</p>
-            <div class="horizontal container project-button-container">
-                { site ? <button class="project-button" onClick={() => openLink({site})}>Site</button> : null }
-                <button class="project-button" onClick={() => openLink({link})}>Source</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="project-card">
+      <h3 className="project-title">{title}</h3>
+      <p className="project-desc">{desc}</p>
+      <div className="horizontal container project-button-container">
+        { site && <button className="project-button" onClick={() => openLink(site)}>Site</button> }
+        <button className="project-button" onClick={() => openLink(link)}>Source</button>
+      </div>
+    </div>
+  );
 }
